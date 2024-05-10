ef7918244dd4e36e9c05c504599ee4a35cc3b7e9
trutikshachoudhary <trutikshachoudhary1707@gmail.com>
2024-05-10 13:29:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "Full Stack Developer",
+      "Web Developer",
+      "Java Developer",
+      "Backend Developer",
+      "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
